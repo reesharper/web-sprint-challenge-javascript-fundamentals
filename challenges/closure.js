0484202e172e2bc9f078a1f94dbb4118ed7fddbc
reesharper <reestimharper@gmail.com>
2020-09-18 12:11:01 -0400

    completed the closure.js file

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,20 @@ myFunction();
 
 // Explanation: 
 
+/* myFunction is creating a local variable in the "internal" variable as well as the new nested function "nestedFunction". nestedFunction doesnt need to have any variables inside of it because it is able to reach out into the lexical scope within "myFunction" as its parent and use the "internal" variable.
+
+*/
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(number){
+  let sum = 0;
+  for (let i = 1; i <= number; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(summation(4));
